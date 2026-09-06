0c2c8ea67a6b9a6991508210c57fc67ffaeba413
Pongpanot <pongpanot_s@jagota.co.th>
2026-09-06 20:34:13 +0700

    feat(api): mount intake/:intakeId/documents routes

--- apps/api/src/documents/documents.module.ts
+++ apps/api/src/documents/documents.module.ts
@@ -1,13 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
+import { IntakeDocumentsController } from './intake-documents.controller';
 import { DocumentPublicationService } from './document-publication.service';
 import { DocumentPublicationController } from './document-publication.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [forwardRef(() => NotificationsModule)],
-  controllers: [DocumentsController, DocumentPublicationController],
+  controllers: [DocumentsController, IntakeDocumentsController, DocumentPublicationController],
   providers: [DocumentsService, DocumentPublicationService],
   exports: [DocumentsService],
 })
